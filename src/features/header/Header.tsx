6806570eb4854decb62395cabb7d850410fd1efc
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2024-09-05 21:43:32 +0300

    fixed_imageSrc_v4

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,10 +1,11 @@
 import styles from './Header.module.css'
+import logo from '../../img/logo.png'
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.nav}>
-                <img src="../../img/logo.png" alt="Logo" />
+                <img src={logo} alt="Logo" />
                 <a className={styles.navLink} href="https://github.com/VioletaNaboy/smartbusiness" target="_blank" rel="noopener noreferrer" aria-label='GitHub link'>JUNIOR FRONTEND DEVELOPER ASSIGNMENT</a>
             </div>
         </header>
